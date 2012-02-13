0a080a7667f248d159dc331e2c8f9c9baeae4345
Willy Tarreau <w@1wt.eu>
2012-02-13 09:31:35 +0100

    BUG: missing parenthesis causing poll events to be checked for nothing

--- httpterm.c
+++ httpterm.c
@@ -2763,7 +2763,7 @@ int poll_loop(int action) {
       for (count = 0; status > 0 && count < nbfd; count++) {
 	  fd = poll_events[count].fd;
 	  
-	  if (!poll_events[count].revents & ( POLLOUT | POLLIN | POLLERR | POLLHUP ))
+	  if (!(poll_events[count].revents & ( POLLOUT | POLLIN | POLLERR | POLLHUP )))
 	      continue;
 
 	  /* ok, we found one active fd */
